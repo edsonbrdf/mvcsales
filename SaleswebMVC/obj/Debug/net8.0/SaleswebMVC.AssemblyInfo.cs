684938f059c23ff5f2d6a2879ef5a67d7f408ef6
Edson <edsonjr.df@gmail.com>
2025-02-24 22:17:07 -0300

    bootstrap

--- SaleswebMVC/obj/Debug/net8.0/SaleswebMVC.AssemblyInfo.cs
+++ SaleswebMVC/obj/Debug/net8.0/SaleswebMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SaleswebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c361840351aa7ce4c72f8594af854a926c1df7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b41ea69dffa64d77ab5e45bd3ec4da7978e974")]
 [assembly: System.Reflection.AssemblyProductAttribute("SaleswebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SaleswebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
